e403b4081e1c92cc5d3fabf573cbe0a053524d82
Sud <sathani@Sudhanvas-MacBook-Pro-2.local>
2016-12-10 20:46:26 -0500

    initial commit

---
+++ LeetCodeProblems/LinkedList/src/Nodes/SingleLLNode.java
@@ -0,0 +1,22 @@
+package Nodes;
+
+public class SingleLLNode {
+
+	int val;
+	SingleLLNode next;
+	
+	public int getVal() {
+		return val;
+	}
+	public void setVal(int val) {
+		this.val = val;
+	}
+	public SingleLLNode getNext() {
+		return next;
+	}
+	public void setNext(SingleLLNode next) {
+		this.next = next;
+	}
+	
+	
+}
